5a23ffc5ac943811397feff9e0cb445f0fc48d4a
lemmani <lemmanuelf@gmail.com>
2023-09-20 15:08:29 -0700

    the is is just a test

---
+++ testing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- it is the principal function
+ *
+ * Return: it will out put 0
+ */
+int main(void)
+{
+	int a = 45;
+
+	printf("this is how old you are %d ", a);
+
+	return (0);
+}
