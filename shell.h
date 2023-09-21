e3ab8a6d4c23d3dacad351e32209a5ad70608bc9
lemmani <lemmanuelf@gmail.com>
2023-09-20 19:06:21 -0700

    the one

--- shell.h
+++ shell.h
@@ -12,22 +12,22 @@
 #include <fcntl.h>
 #include <errno.h>
 
-// for read/write buffers
+/* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-// for command chaining
+/* for command chaining */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
 #define CMD_CHAIN	3
 
-// for convert_number()
+/* for convert_number() */
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
 
-// 1 if using system getline()
+/* 1 if using system getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
@@ -38,7 +38,7 @@ extern char **environ;
 
 
 /**
- * struct liststr - singly linked list in the code
+ * struct liststr - singly linked list
  * @num: the number field
  * @str: a string
  * @next: points to the next node
@@ -52,7 +52,7 @@ typedef struct liststr
 
 /**
  * struct passinfo - contains pseudo-arguements to pass into a function,
- * allowing uniform prototype for function pointer structure
+ * allowing uniform prototype for function pointer struct
  * @arg: a string generated from getline containing arguements
  * @argv:an array of strings generated from arg
  * @path: a string path for the current command
@@ -211,21 +211,21 @@ int read_history(info_t *info);
 int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
-// toem_lists.c
+/* toem_lists.c */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
-// toem_lists1.c
+/* toem_lists1.c */
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
 
-// toem_vars.c
+/* toem_vars.c */
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
